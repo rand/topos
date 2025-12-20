674b6dae0b74af91f11b10fa3233ab4ece82e6d0
Rand Arete <rand.arete@gmail.com>
2025-12-20 15:35:35 -0700

    feat(analysis): Integrate tree-sitter-topos

--- crates/topos-analysis/src/lib.rs
+++ crates/topos-analysis/src/lib.rs
@@ -1,7 +1,18 @@
+pub fn language() -> tree_sitter::Language {
+    tree_sitter_topos::language()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert!(true);
+    fn test_language_load() {
+        // This will currently panic because unimplemented!() in tree-sitter-topos
+        // but it verifies the linkage.
+        let result = std::panic::catch_unwind(|| {
+            language();
+        });
+        assert!(result.is_err());
     }
 }
