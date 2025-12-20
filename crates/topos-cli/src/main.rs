c3339d33546b8f7e6277596acc082963c92e072c
Rand Arete <rand.arete@gmail.com>
2025-12-20 11:23:50 -0700

    feat(cli): Add lsp subcommand to launch server

--- crates/topos-cli/src/main.rs
+++ crates/topos-cli/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, Topos!");
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Start the Language Server
+    Lsp,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Lsp => {
+            topos_lsp::run_server().await;
+        }
+    }
+
+    Ok(())
 }
